089e70b7b13f2f7b84ed0c2fdf627143e9d0088c
raul398 <raul398@hotmail.com.ar>
2021-04-20 23:57:21 -0300

    initial commit

---
+++ proyectoweb/proyectowebapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def home(request):
+
+    return render(request, 'proyectowebapp/home.html')
+
+
+def tienda(request):
+
+    return render(request, 'proyectowebapp/tienda.html')
+
---
+++ proyectoweb/servicios/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Servicio(models.Model):
+    titulo = models.CharField(max_length=50)
+    contenido = models.CharField(max_length=50)
+    imagen = models.ImageField(upload_to='servicios')
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = 'Servicio'
+        verbose_name_plural = 'Servicios'
+
+    def __str__(self):
+        return self.titulo
